a0a7cd3b094527c8690463d65991fc94c294ac6b
jeong moon ho <kz1324@naver.com>
2020-11-10 20:12:51 +0900

    '20201110'

---
+++ navigator/lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'navigator',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: FirstPage(),
+    );
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context2) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First page'),
+      ),
+      body: Center(
+        child: RaisedButton(
+            child: Text('go to the second page'),
+            onPressed: () {
+              Navigator.push(context2,
+                  MaterialPageRoute(builder: (context) => SecondPage()));
+            }),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext ctx) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second page'),
+      ),
+      body: Center(
+        child: RaisedButton(
+            child: Text('go to the first page'),
+            onPressed: () {
+              Navigator.pop(ctx);
+            }),
+      ),
+    );
+  }
+}
